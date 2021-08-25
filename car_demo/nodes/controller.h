59e75a0944e9ade0fab37f18098fe625d82169ed
Yusuf Can Aydemir <can.aydemir@metu.edu.tr>
2021-08-25 13:51:54 +0300

    Changed controller node it only publish a speed

--- car_demo/nodes/controller.h
+++ car_demo/nodes/controller.h
@@ -3,22 +3,7 @@
 
 #include"ros/ros.h"
 #include"prius_msgs/Control.h"
-#include"sensor_msgs/LaserScan.h"
-#include"nav_msgs/Odometry.h"
-#include"geometry_msgs/Vector3.h"
-#include"math.h"
 
 #define DESIRED_SPEED 3
-
-struct PID_parameters {
-    float   kp ;
-    float   kd ;
-    float   ki ;
-};
-
-struct PID_History {
-    float   error_one_timestamp_before;
-    float   error_sum;
-};
 
 #endif   /*CONTROLLER_H_*/
